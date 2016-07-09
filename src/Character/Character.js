bb47a6fe9dada4a348f13a42d363cf27b0fb1a79
jake-freeman <jfreeman@hawk.iit.edu>
2016-07-08 21:32:50 -0500

    Add initial character class

---
+++ src/Character/Character.js
@@ -0,0 +1,25 @@
+"use strict"
+/* Character class */
+
+module.exports = class Character
+{
+  /**
+   * @param {Object} config        describes starting state of character
+   * @param {String} config.name   character name
+   * @param {Object} config.stats  the core statistics that decribe the character
+   */
+  constructor(config)
+  {
+    this.config = config;
+  }
+
+  get name()
+  {
+    return this.config.name;
+  }
+
+  get stats()
+  {
+    return this.config.stats;
+  }
+}
