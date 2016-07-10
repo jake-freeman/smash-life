d16e485e9bf06d56531099457ce24719791c59d8
jake-freeman <jfreeman@hawk.iit.edu>
2016-07-10 15:00:00 -0500

    Add stat group and SmashGame definition + add top 6 melee character stats

---
+++ src/Stats/StatGroup.js
@@ -0,0 +1,30 @@
+"use strict"
+/* StatGroup class */
+
+module.exports = class StatGroup
+{
+  /**
+   * @param {Object} config        describes starting state of the stat group
+   * @param {String} config.name   Name of the stat group
+   * @param {Object} config.stats  The `name: value` pairs that make up the stat group
+   *
+   */
+  constructor(config)
+  {
+    this.config = config;
+  }
+
+  get name()
+  {
+    return this.config.name;
+  }
+
+  /**
+   * @param {String} name  Name of the specific stat
+   *
+   * @returns {value}  The value of the object
+   */
+  stat(name) {
+    return this.config.stats[name];
+  }
+}
