f90506151ba3a473915e1969e8a6618db018fa94
jake-freeman <jfreeman@hawk.iit.edu>
2016-09-04 16:44:46 -0500

    US10, TA8: Create basic stub for Battle class

--- src/Competitor/Competitor.js
+++ src/Competitor/Competitor.js
@@ -6,12 +6,12 @@ const Base = require('../core/Base.js');
 module.exports = class Competitor extends Base
 {
   /**
-   * constructor variables
-   * @param {Object} config        describes starting state of a player
-   * @param {String} config.name   real name
-   * @param {String} config.tag    gamertag
-   * @param {Array}  config.mains  string array of mains
-   * @param {Object} config.stats  object that has all stats in it
+   * @param {Object}    config               describes starting state of a player
+   * @param {String}    config.name          real name
+   * @param {String}    config.tag           gamertag
+   * @param {Array}     config.mains         string array of mains
+   * @param {Character} config.current_char  Character competitor is currently using
+   * @param {Object}    config.stats         object that has all stats in it
    */
   constructor(config)
   {
